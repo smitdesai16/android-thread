19bbfc94da9a0a4be0ce3a2100632dc6a411193c
Smit Desai <smit.desai16@gmail.com>
2020-06-19 15:57:59 -0500

    Running high load task on thread

--- app/src/main/java/me/creatorguy/androidthread/MainActivity.java
+++ app/src/main/java/me/creatorguy/androidthread/MainActivity.java
@@ -1,8 +1,11 @@
 package me.creatorguy.androidthread;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -11,6 +14,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private static TextView tvMainHighLoadTaskResult;
     private static Button btnMainPerformHighLoadTask;
+
+    Handler handler = new Handler() {
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+            //based on high load task result
+            tvMainHighLoadTaskResult.setText("Task is complete.");
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,21 +39,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (view.getId()) {
             case R.id.btnMainPerformHighLoadTask:
 
-                // high load task start
-                long futureTime = System.currentTimeMillis() + 10000;
-                while (System.currentTimeMillis() < futureTime) {
-                    synchronized (this) {
-                        try {
-                            wait(futureTime - System.currentTimeMillis());
-                        }catch (Exception e) {
-                            e.printStackTrace();
+                Runnable runnable = new Runnable() {
+                    @Override
+                    public void run() {
+                        // high load task start
+                        long futureTime = System.currentTimeMillis() + 10000;
+                        while (System.currentTimeMillis() < futureTime) {
+                            synchronized (this) {
+                                try {
+                                    wait(futureTime - System.currentTimeMillis());
+                                }catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+                            }
                         }
-                    }
-                }
-                // high load task end
+                        // high load task end
 
-                //based on high load task result
-                tvMainHighLoadTaskResult.setText("Task is complete.");
+                        // you cannot make ui thread level changes from thread. instead use ui thread using handler.
+                        handler.sendEmptyMessage(0);
+                    }
+                };
+
+                Thread thread = new Thread(runnable);
+                thread.start();
+
                 break;
             default:
                 break;
